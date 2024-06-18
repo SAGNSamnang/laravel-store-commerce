63822068ab2aab9ba686d24b23b3b59c9a23592c
SAGNSamnang <110118114+SAGNSamnang@users.noreply.github.com>
2024-06-19 00:43:56 +0700

    Create HomeController and make route to admin dashboare, create new index in view/admin and lastly make condition to check in AuthenticatedSessionController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route for admin 
+Route::get('admin/dashboard', [HomeController::class, 'index']);
